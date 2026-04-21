e03a8f3fc658b8c4333efbb00a5c744874628dc0
S1RGOVED <rodolfo.roldao@dxc.com>
2026-04-21 10:29:30 +0300

    changes for parameterization

--- api_flask_test/test_datadriven_csv.py
+++ api_flask_test/test_datadriven_csv.py
@@ -1,5 +1,5 @@
 import pytest
-from utils.file_utils import get_csv_from_file
+from utils.file_utils import get_csv_from_file, get_data_as_tuple
 from utils.api_utils import get_api_data, post_api_data, delete_api_data
 from utils.config_reader import get_flask_app
 
@@ -11,6 +11,7 @@ url_register = base_uri + url_path_register
 url_delete = base_uri + url_path_delete
 url_path_login = "login"
 url_login = base_uri + url_path_login
+data_file_with_status = "register_api_data_with_status.csv"
 
 
 def test_data_driven():
@@ -19,8 +20,6 @@ def test_data_driven():
     for payload in payload_list:
         resp = post_api_data(url_register, payload)
         assert resp.status_code == 201
-
-
 
 
 @pytest.mark.parametrize("line", data_file)
@@ -47,3 +46,16 @@ def test_driven_param(line):
     delete_status = delete_api_data(url_delete, user, token)
 
     assert delete_status.status_code == 200
+
+
+get_data = get_data_as_tuple(data_file_with_status)
+
+
+@pytest.mark.parametrize("input, resp_status", get_data)
+def test_data_driven2(input, resp_status):
+    url = base_uri + url_path_register
+    keys = ["email", "password"]
+    request_dict = dict(zip(keys, input))
+    print("Request dict:", request_dict, resp_status)
+    resp = post_api_data(url, request_dict)
+    assert resp.status_code == int(resp_status)
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -25,3 +25,34 @@ def get_csv_from_file(file_name):
 
 
 getcsv = get_csv_from_file("register_api_data.csv")
+
+
+# get data from CSV as lsit
+def get_data_as_list(file_name):
+    file_path = test_data_dir / file_name
+    with open(file_path, "r") as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        lines = list(reader)
+    return lines
+
+
+print(get_data_as_list("register_api_data_with_status.csv"))
+
+
+# returns list of tuples, within tuples it's "list of inputs" and a scalar value for output-status
+def get_data_as_tuple(file_name):
+    data_list = get_data_as_list(file_name)
+    list_of_tuples = []
+
+    for line in data_list:
+        line_tuple = (line[:2], line[2])
+        list_of_tuples.append(line_tuple)
+    # lsit comprehension
+    new_list_2 = [(x[:2], x[2]) for x in data_list]
+    return new_list_2
+
+print(get_data_as_tuple("register_api_data_with_status.csv"))
+
+# we can create a dict with list of zipped keys and values
+
